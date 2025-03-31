fa56586c366ac140e07282f376f4cd3d6a7bb03d
Николай Васильев <nvasiljev1977@gmail.com>
2025-03-31 18:44:49 +0400

    Task1

--- Tyuiu.VasilevNV.Sprint3.Task1.V13.Lib/DataService.cs
+++ Tyuiu.VasilevNV.Sprint3.Task1.V13.Lib/DataService.cs
@@ -3,34 +3,25 @@ namespace Tyuiu.VasilevNV.Sprint3.Task1.V13.Lib
 {
     public class DataService : ISprint3Task1V13
     {
-        public double GetMultiplySeries(double value, int startValue, int stopValue)
+        public double GetMultiplySeries(double value, int startValue = 1, int stopValue = 7)
         {
             if (startValue > stopValue)
-                throw new ArgumentException("Начальное значение не может быть больше конечного");
+                throw new ArgumentException("Начальное значение не может быть больше конечного", nameof(startValue));
 
             if (startValue <= 0)
-                throw new ArgumentException("Начальное значение должно быть положительным");
+                throw new ArgumentException("Начальное значение должно быть положительным", nameof(startValue));
 
             if (Math.Abs(value) < double.Epsilon)
-                throw new ArgumentException("Значение value не может быть нулевым");
+                throw new ArgumentException("Значение value не может быть нулевым", nameof(value));
 
             double product = 1.0;
-
             int k = startValue;
+
+           
             while (k <= stopValue)
             {
-
-                double term = Math.Pow(value / k, 3);
-
-                if (Math.Abs(product) < 1e-100 && term != 0)
-                {
-                    double logProduct = Math.Log10(product) + 3 * Math.Log10(value / k);
-                    product = Math.Pow(10, logProduct);
-                }
-                else
-                {
-                    product *= term;
-                }
+             
+                product *= Math.Pow(value, k);
 
                 k++;
             }
@@ -39,4 +30,3 @@ namespace Tyuiu.VasilevNV.Sprint3.Task1.V13.Lib
         }
     }
 }
-        
